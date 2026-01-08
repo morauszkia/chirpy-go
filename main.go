adb648a356ff8a613ff219000b2878ce86a291ca
morauszkia <morauszki.andras@gmail.com>
2026-01-08 19:00:31 +0100

    Add static file serving & readiness endpoint

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 func main() {
 	port := "8080"
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	server := &http.Server{
 		Addr:   ":" + port,
