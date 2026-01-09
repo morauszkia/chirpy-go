dc4fefcbe2b11fa298fb84a7da25a87c703fff2f
morauszkia <morauszki.andras@gmail.com>
2026-01-09 18:23:30 +0100

    Register stateful middleware & limit methods

--- main.go
+++ main.go
@@ -3,17 +3,50 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
+	"fmt"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Metrics reset \n"))
+}
+
+func (cfg *apiConfig) getHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	metrics := fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())
+	w.Write([]byte(metrics))
+}
 
 func main() {
 	port := "8080"
+	apiCfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("GET /metrics", apiCfg.getHits)
+
+	mux.HandleFunc("POST /reset", apiCfg.reset)
 
 	server := &http.Server{
 		Addr:   ":" + port,
